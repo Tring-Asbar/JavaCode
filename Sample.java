1a7be996661d111878a1d703ad35e7e419fbdc38
Tring-Asbar <asbar.j@tringapps.com>
2025-01-22 17:13:46 +0530

    Added Loops

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
     public static void main(String[] args){
-        System.out.print("Hello Tringapps!!!");
+        System.out.print("Hello Tringapps Interns!!!");
         
     }
 }
